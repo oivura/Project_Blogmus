e5b1282f6fc085e015aa8146305eb77aead14a13
oivura <petersthanlie1026@gmail.com>
2024-06-23 22:58:59 +0800

    Update Models vers 2

---
+++ app/Models/Tag.php
@@ -0,0 +1,23 @@
+<?php 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+
+class Tag extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $keyType = 'uuid';
+    public $incrementing = false;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    public function articles()
+    {
+        return $this->belongsToMany(Article::class, 'article_tags');
+    }
+}
